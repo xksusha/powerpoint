367dba8b6c231e52ba02f057cea8d2b2b4d37b31
Ksenia Kanishcheva <xksusha@ksenias-mbp.eki.group>
2016-06-08 11:37:36 +0200

    trying to do something

--- test/test_upgrade.rb
+++ test/test_upgrade.rb
@@ -1,5 +1,5 @@
 require 'eki-powerpoint'
 	@deck = EkiPowerpoint::Presentation.new
 	@deck.add_intro 'Mediadashboard', 'Overview export'
-	@deck.add_overview_slide 'Devices', 'samples/images/image12.png', {'Lorem'=>'ff0000', 'Ipsum'=> '40ff00', 'Neque porro quisquam'=> 'ff00ff' }
+	@deck.add_overview_slide 'Devices', 'samples/images/sample_png.png', {'Lorem'=>'ff0000', 'Ipsum'=> '40ff00', 'Neque porro quisquam'=> 'ff00ff' }
 	@deck.save('samples/pptx/new_sample.pptx')
